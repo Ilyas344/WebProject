c7c640d45615efc0e63661764f3608c4bc708349
Ilyas <xxxilyas92xxx@gmail.com>
2023-02-01 00:24:02 +0500

    Введение в Maven и сборку проектов

--- src/main/java/com/example/webproject/controllers/FirstController.java
+++ src/main/java/com/example/webproject/controllers/FirstController.java
@@ -1,2 +1,21 @@
-package com.example.webproject.controllers;public class FirstController {
+package com.example.webproject.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping()
+public String student(){
+    return "Приложение запущено";
 }
+    @GetMapping("/info")
+public String info(){
+        return "Имя ученика: Кучукбаев Ильяс, Название проекта: Изучение Spring Java, дата:1.02.2023 00:02 \n"+
+                "Название проекта: Введение в Maven и сборку проектов "+
+                "Функция: Web разработка "+
+                "Технологии: Spring "+
+                "Язык: java";
+};
+}
+
